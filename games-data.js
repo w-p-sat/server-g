e9fd1ee7c8bb2af8fae9df43ac2f9f10235fe2d1
Ruzke <146598189+w-p-sat@users.noreply.github.com>
2025-09-06 15:43:42 +0300

    Add files via upload

---
+++ games-data.js
@@ -0,0 +1,6 @@
+export const games = [
+    { id: "book_of_ra", name: "Book of Ra" },
+    { id: "lucky_lady", name: "Lucky Lady's Charm" },
+    { id: "sizzling_hot", name: "Sizzling Hot" },
+    { id: "gonzos_quest", name: "Gonzo's Quest" }
+];
